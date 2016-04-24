6e475b3e3e6aa4f739924c910bca2ad26d2f1664
wyxmg <ejs.important@gmail.com>
2016-04-23 22:11:45 -0400

    Basic Scrapy settings + MongoDB location input

---
+++ vg/settings.py
@@ -0,0 +1,9 @@
+BOT_NAME = 'vg'
+
+SPIDER_MODULES = ['vg.spiders']
+NEWSPIDER_MODULE = 'vg.spiders'
+
+MONGODB_SERVER = "localhost"
+MONGODB_PORT = 27017
+MONGODB_DB = "devposts"
+MONGODB_COLLECTION = "devposts"
